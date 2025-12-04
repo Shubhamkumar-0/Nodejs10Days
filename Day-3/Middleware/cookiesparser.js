9f77229d63781f5d197e79183b227eca8c7b3fa0
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-12-04 17:51:56 +0000

    creting file and foolder

---
+++ Day-3/Middleware/cookiesparser.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.use(cookieParser()); // default: unsigned cookies
+
+app.get('/set-cookie', (req, res) => {
+  res.cookie('username', 'Shubham'); // cookie name = 'username', value = 'Shubham'
+  res.send('Cookie has been set!');
+});
+app.get('/get-cookie', (req, res) => {
+  const username = req.cookies.username; // read cookie
+  res.send(`Hello, ${username}`);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000/users/form");
+});
